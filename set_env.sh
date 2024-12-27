72f8c450d1b9a9f17ac10395b236b201cef0bff2
hungptit2809 <hungptit2809@gmail.com>
2024-12-28 02:05:44 +0700

    init 01

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=hungho2809
+export POSTGRES_HOST=database-1.colkls8c2xky.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::bucket-images-01
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
